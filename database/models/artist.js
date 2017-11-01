b1ccd74dbfeef9350b14a8954fac3125397991eb
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-11-01 22:30:48 +0800

    UpstarMusic 12-74: The Arists Schema

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AlbumSchema = require('./album');
 
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [ AlbumSchema ]
+});
+
+const User = mongoose.model('user', ArtistSchema);
+
+module.export = User;
