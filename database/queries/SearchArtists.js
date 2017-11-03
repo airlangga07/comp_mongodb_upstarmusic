7919fd483013d41a8b06ab89483260a7cf42bb26
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-11-03 22:58:28 +0800

    UpstarMusic 12-86: Sorting, Limiting, and Skipping Together

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,19 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  // ES6 NEW PROPERTY INTERPOLATION
+  // instead of doing this
+  // let sortOrder = {};
+  // sortOrder[sortProperty] = 1;
+  // we can do this: { [sortProperty]: 1 }
+  const query = Artist
+    .find({})
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()])
+    .then(results => {
+      return { all: results[0], count: results[1], offset, limit };
+    });
 };
