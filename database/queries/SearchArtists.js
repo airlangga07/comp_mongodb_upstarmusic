2a0c8908cc10a5b3606db78dac8e7ecc598399d8
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-11-04 12:14:14 +0800

    UpstarMusic 13-90: Handling text search

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -29,8 +29,23 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
 const buildQuery = (criteria) => {
   const query = {};
 
-  if (criteria.age) query.age = { $gte: criteria.age.min, $lte: criteria.age.max };
-  if (criteria.yearsActive) query.yearsActive = { $gte: criteria.yearsActive.min, $lte: criteria.yearsActive.max };
+  if (criteria.age) {
+    query.age = { 
+      $gte: criteria.age.min, 
+      $lte: criteria.age.max 
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = { 
+      $gte: criteria.yearsActive.min, 
+      $lte: criteria.yearsActive.max 
+    };
+  }
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
   
   return query;
 }; 
